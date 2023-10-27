9840b57429bb7e08d0d70a6562ad961478adcb1e
Stepen Stepen <stepen@sindata.net>
2023-10-27 16:23:33 +0800

    feat: update e2e testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -17,19 +17,33 @@
 
 //   it('/ (GET)', () => {
 //     return request(app.getHttpServer())
-//       .get('/')
+//       .get('/')w
 //       .expect(200)
 //       .expect('Hello World!');
 //   });
-// });
+// });a
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import {Test} from '@nestjs/testing';
-import { AppModule } from '../src/app.module';
+import { AppModule } from 'src/app.module';
 
 describe('App e2e', () => {
+  let app: INestApplication;
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
+    app = moduleRef.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe
+      ({
+        whitelist: true,
+      }),
+    );
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
   it.todo('should pass');
 });
